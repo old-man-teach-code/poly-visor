1b93026e735a3d42b444a509b704b22b7642dd57
MinhBui2002 <heribertschneller@gmail.com>
2022-09-29 16:19:11 +0700

    basic API for information and status of Supervisor

---
+++ app.py
@@ -0,0 +1,76 @@
+import re
+from unittest import result
+from flask import Flask, render_template, Response, request, json, jsonify, session
+from markupsafe import escape
+import json
+from sup_information import get_hostname, process_PID, process_readLogFile_out, process_swap, sup_Identification, sup_State, process_AllInfo, process_memory_usage, process_Info
+from flask_cors import CORS
+# ...
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/capitalize/<word>/')
+def capitalize(word):
+    return jsonify({'word': word.capitalize()})
+
+
+@app.route('/api/state')
+def returnState():
+    return jsonify((sup_State()))
+
+
+@app.route('/api/allProcessInfo')
+def returnAllProcessInfo():
+    return jsonify((process_AllInfo()))
+
+# get process info by name
+
+
+@app.route('/api/processInfo/<name>')
+def returnProcessInfo(name):
+    return jsonify((process_Info(name)))
+
+# get process_memory_usage
+@app.route('/api/processMemoryUsage/<pid>')
+def returnProcessMemoryUsage(pid):
+    result = process_memory_usage(pid)
+    array_result = [line.split() for line in result.splitlines()]
+    array_result = list(zip(*array_result))
+    return jsonify(dict(array_result))
+
+
+# get the swap memory used by the process
+@app.route('/api/processSwap/<pid>')
+def returnProcessSwap(pid):
+    result = process_swap(pid)
+    # remove "kb" at the end of the string
+    result = result.replace("kB", "")
+    # convert the result to json with the format {"key": "value"}
+    result = (dict([line.split() for line in result.splitlines()]))
+    return jsonify(result)
+
+
+# get host name
+@app.route('/api/hostname')
+def returnHostname():
+    # return the result with the format {"Host name": "value"}
+    result = get_hostname()
+    result = result.replace("\n", "")
+    return jsonify({"Host name": result})
+
+# get identifications
+@app.route('/api/identifications')
+def returnIdentifications():
+    result = sup_Identification()
+    return jsonify(result)
+
+
+# get PID by name
+@app.route('/api/pid/<name>')
+def returnPid(name):
+    result = process_PID(name)
+    return jsonify({name: result})
+
+
