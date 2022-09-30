26186f7a93251ba96e0354869035501229ca281f
Hoang Long <62252259+hlk9@users.noreply.github.com>
2022-10-01 03:41:47 +0700

    get path of process, config file and server address

--- finder.py
+++ finder.py
@@ -1,17 +1,50 @@
+from ctypes.wintypes import PLARGE_INTEGER
+from distutils.command import config
+from importlib.resources import path
 import os
 import re
+import configparser
+from procstatus import process_PID
 
 #Get config file path of Supervisord
 def get_sup_config_path():
+    output=""
     stream = os.popen("""ps ux | grep -P '^(?=.*.conf)(?=.*supervisord)' | head -n -2""")
     output = stream.read()
+    if("supervisord" not in output):
+        stream2 = os.popen("""ps ux | grep -P '^(?=.*.ini)(?=.*supervisord)' | head -n -2""")
+        output = stream2.read() 
+   
     s = re.findall(r'(\/.*?\.[\w:]+)', output)
-    lis = s[1].split()
+    try:
+        lis = s[1].split()
+    except: return "Supervisord is not running"
+    
     path=""
     for x in lis:
-        if(r".conf"in x):
+        if(r".conf"in x or r".ini" in x):
             path+=x
-    if path=="" or not path :
-        path="Can't find config path of Supervisord"   
-        
+    if path=="" or not path:
+        path="Can't find config path of Supervisord"
     return path
+
+#Get include process config files path
+def get_proc_config_path():
+    config = configparser.ConfigParser()    #Can't read more one line
+    config.read(get_sup_config_path())
+    path = config['include']['files']    
+    
+    return path
+
+#Get serverurl Supervisor
+def get_sup_serverurl():
+    parser_file = configparser.ConfigParser()
+    parser_file.read(get_sup_config_path())
+    sup_url= parser_file['supervisorctl']['serverurl']   #Conflict when two more line in .conf file, work well on .ini file
+    return sup_url
+
+#Get path of process with pid when it running
+def get_path_proc(proc_PID):
+    stream = os.popen("readlink -f /proc/"+str(proc_PID)+"/exe")
+    output = stream.read()
+    return  output
