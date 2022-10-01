966728428532a3b0021720926d5d8a079b50d78f
Hoang Long <62252259+hlk9@users.noreply.github.com>
2022-10-01 14:32:37 +0700

    get data from supervisord configfile

--- machinestatus.py
+++ machinestatus.py
@@ -1,7 +1,16 @@
+from ctypes.wintypes import PLARGE_INTEGER
+from time import sleep, thread_time
 from xmlrpc.client import ServerProxy
 import os
 
-server = ServerProxy('http://localhost:10019/RPC2')
+from urllib3 import Retry
+from finder import get_sup_serverurl, get_sup_config_path
+
+#server = ServerProxy('http://localhost:10019/RPC2')
+
+#Server can be auto set by read logfile
+server = ServerProxy(str(get_sup_serverurl())+"/RPC2")
+
 
 #Get Supervior State 
 def sup_State() :
@@ -63,9 +72,18 @@ def get_memory_status():
     output = stream.read()
     return  output
 
-#Get total memory spec GB
+#Get total memory, CPU model, processor spec GB
 def get_machine_spec():
     stream = os.popen("""cat /proc/cpuinfo | grep 'model name' | uniq && free -g -h -t | grep Mem | awk '{print "Total Memory: " $2}'""")
     output = stream.read()
     output+= "CPUs: "+str(os.cpu_count())
     return  output.replace("model name","CPU Model Name")
+
+#Get data from config file of supervisord
+def get_data_sup_config_file(path_file):
+    fil = open(path_file,"r")
+    data = fil.read()
+    fil.close()
+    return data
+
+
