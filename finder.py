a34b4ee771553786bb6f93f065fde5032c650f89
MinhBui2002 <heribertschneller@gmail.com>
2022-10-03 15:26:58 +0700

    pull test

---
+++ finder.py
@@ -0,0 +1,69 @@
+from collections import OrderedDict
+import os
+import re
+import configparser
+
+
+#To get multiple value in config file
+class MultiOrderedDict(OrderedDict):
+    def __setitem__(self, key, value):
+        if isinstance(value, list) and key in self:
+            self[key].extend(value)
+        else:
+            super().__setitem__(key, value)
+
+#Get config file path of Supervisord when it running on machine
+def get_sup_config_path():
+    output=""
+    stream = os.popen("""ps ux | grep -P '^(?=.*.conf)(?=.*supervisord)' | head -n -2""")
+    output = stream.read()
+    if("supervisord" not in output):
+        stream2 = os.popen("""ps ux | grep -P '^(?=.*.ini)(?=.*supervisord)' | head -n -2""")
+        output = stream2.read() 
+   
+    s = re.findall(r'(\/.*?\.[\w:]+)', output)
+    try:
+        lis = s[1].split()
+    except: return "Supervisord is not running"
+    
+    path=""
+    for x in lis:
+        if(r".conf"in x or r".ini" in x):
+            path+=x
+    if path=="" or not path:
+        path="Can't find config path of Supervisord"
+    return path
+
+supervisord_config_path = get_sup_config_path()
+
+#Get include process config files path
+def get_proc_config_path():
+    config = configparser.RawConfigParser(dict_type=MultiOrderedDict, strict=False)    #Can't read more one line
+    config.read(get_sup_config_path())
+    #path = config['include']['files']   
+    path = config.get("include","files")
+    
+    return path
+
+#Get serverurl Supervisor
+def get_sup_serverurl():
+    parser_file = configparser.RawConfigParser(dict_type=MultiOrderedDict, strict=False)   
+    parser_file.read(get_sup_config_path())
+    sup_url= parser_file.get("supervisorctl","serverurl") #Conflict when two more line in .conf file, work well on .ini file
+    return str(sup_url)
+
+#Get path of process with pid when it running
+def get_path_proc(proc_PID):
+    from procstatus import process_PID
+    stream = os.popen("readlink -f /proc/"+str(proc_PID)+"/exe")
+    output = stream.read()
+    return  output
+
+#Get path of logfile supervisord
+def get_path_sup_logfile():
+    config = configparser.RawConfigParser(dict_type=MultiOrderedDict, strict=False)   
+    config.read(supervisord_config_path)
+    path = config.get("supervisord","logfile")
+    return path
+
+print(get_sup_serverurl())
---
+++ machinestatus.py
@@ -0,0 +1,78 @@
+from xmlrpc.client import ServerProxy
+import os
+from finder import get_sup_serverurl
+
+#server = ServerProxy('http://localhost:10019/RPC2')
+
+#Server can be auto set by read logfile
+server = ServerProxy(str(get_sup_serverurl())+"/RPC2")
+
+
+#Get Supervior State 
+def sup_State() :
+    return server.supervisor.getState()
+
+#Get Supervisor PID
+def sup_PID():
+    return server.supervisor.getPID()
+
+#Get Supervisor Log ()
+def sup_readAllLog():
+    print(server.supervisor.readLog(0,0))
+
+#Get Supervisor API Version
+def sup_APIVer():
+    return server.supervisor.getAPIVersion()
+
+#Get Superivsor Version
+def sup_Version():
+    return server.supervisor.getSupervisorVersion()
+
+#Get Supervisor Indentification
+def sup_Indentification():
+    return server.supervisor.getIdentification()
+
+#Get Information of all process in machine
+def process_AllInfo():
+    return server.supervisor.getAllProcessInfo()
+
+#Get Host Name
+def get_hostname():
+    stream = os.popen("hostname")
+    output = stream.read()
+    return output
+
+#Get current each core CPU Usage
+def get_each_cpu_usage():
+    stream = os.popen("""top 1 -bn1  | grep '^%Cpu' |awk '{print $1,$2,$3"\\n"$18,$19,$20,$21}'""")
+    output = stream.read()    
+    b = output.replace("st ","")               #Output cpu stats
+    result=b.replace(" us,","")
+    return  result
+
+#Get current CPU Usage
+def get_current_cpu_usage():
+    stream = os.popen("""top -bn 1  | grep '^%Cpu' | tail -n 1 | awk '{print $2"%"}'""")
+    output = stream.read()
+    return  output
+
+#Get Memory status
+def get_memory_status():
+    stream = os.popen("""free -g -h -t | grep Mem | awk '{print ($3/$2) * 100"%"}'""")
+    output = stream.read()
+    return  output
+
+#Get total memory, CPU model, processor spec GB
+def get_machine_spec():
+    stream = os.popen(
+        """cat /proc/cpuinfo | grep 'model name' | uniq && free -g -h -t | grep Mem | awk '{print "TotalMemory: " $2}'""")
+    output = stream.read()
+    output += "CPUs: "+str(os.cpu_count())
+    return output.replace("model name", "CPUModelName")
+
+#Get data from config file of supervisord
+def get_data_sup_config_file(path_file):
+    fil = open(path_file,"r")
+    data = fil.read()
+    fil.close()
+    return data
