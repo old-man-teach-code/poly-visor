a324865331e787025680de2c8ce8ea12c41b1c0c
MinhBui2002 <heribertschneller@gmail.com>
2022-10-16 15:26:06 +0700

    Implement logging exception

--- controllers/api.py
+++ controllers/api.py
@@ -9,7 +9,8 @@ import logging
 app = Flask(__name__)
 CORS(app)
 
-
+logging.basicConfig(filename='storage/logs/api.log',level=logging.DEBUG,format='%(asctime)s %(levelname)s %(name)s %(message)s')
+logger=logging.getLogger(__name__)
 
 #get all processes and return a json object
 try:
@@ -21,7 +22,7 @@ try:
             list_of_processes_json.append(process.__dict__)
         return jsonify(list_of_processes_json)
 except Exception as e:
-    print(e)
+    logger.exception(e)
 
 #get supervisor object and return a json object
 try:
@@ -30,7 +31,7 @@ try:
         supervisor = get_supervisor()
         return jsonify({'stateName':supervisor.stateName,'stateCode':supervisor.stateCode, 'pid':supervisor.pid})
 except Exception as e:
-    print(e)        
+    logger.exception(e)     
 
 
 #get system object and return a json object
@@ -40,4 +41,6 @@ try:
         system = get_system()
         return jsonify({'cpu': system.current_cpu_usage,'cores':system.each_cpu_usage, 'memory':system.memory_status, 'machineSpec':system.machine_spec})
 except Exception as e:
-    print(e)
+    #logging the exception to a test.log file in the storage/logs folder
+    logger.exception(e)
+    
