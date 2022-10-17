ef76e3d0a14e6cc6cff7f41a47f455d4d39d95ff
Hoang Long <62252259+hlk9@users.noreply.github.com>
2022-10-17 23:16:58 +0700

    change to runShell func

--- models/modelSystem.py
+++ models/modelSystem.py
@@ -11,34 +11,31 @@ class System:
 
     @property
     def each_cpu_usage(self): #Get stats of each core CPU 
-        stream = os.popen(
-            """top 1 -bn1  | grep '^%Cpu' |awk '{print $1,$2,$3"\\n"$18,$19,$20,$21}'""")
-        output = stream.read()
+        output = runShell("""top 1 -bn1  | grep '^%Cpu' |awk '{print $1,$2,$3"\\n"$18,$19,$20,$21}'""")
         b = output.replace("st ","")               
         result=b.replace(" us,","")
         result = result.replace("\n", " ")
         result = result.replace("us,", "")
         result = result.replace(":", " ")
         result = dict(zip(result.split()[::2], result.split()[1::2]))
-        return result     
+        return result  
+
     @property
     def current_cpu_usage(self):
-        stream = os.popen("""top -bn 1  | grep '^%Cpu' | tail -n 1 | awk '{print $2"%"}'""")
-        output = stream.read()  
-        output = output.replace("\n", "")        
-        return output
+        output = runShell("""top -bn 1  | grep '^%Cpu' | tail -n 1 | awk '{print $2"%"}'""")
+        result = output.replace("\n", "")        
+        return result
+
     @property
     def memory_status(self):
-        stream = os.popen(
-            """free -g -h -t | grep Mem | awk '{print ($3/$2) * 100"%"}'""")
-        output = stream.read()
-        output = output.replace("\n", "")
-        return output
+        output = runShell( """free -g -h -t | grep Mem | awk '{print ($3/$2) * 100"%"}'""")
+        result = output.replace("\n", "")
+        return result
 
     @property
     def machine_spec(self):
-        result = runShell("""cat /proc/cpuinfo | grep 'model name' | uniq && free -g -h -t | grep Mem | awk '{print "TotalMemory: " $2}'""")
-        result = result.replace("model name", "CPU")
+        output = runShell("""cat /proc/cpuinfo | grep 'model name' | uniq && free -g -h -t | grep Mem | awk '{print "TotalMemory: " $2}'""")
+        result = output.replace("model name", "CPU")
         result += "CPUs: "+str(os.cpu_count())
         result = result.replace("\t", "")
         result = (dict([line.split(': ') for line in result.splitlines()]))
