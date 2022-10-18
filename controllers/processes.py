41cf1a9e6e4c01493d5a27432767b74ca54724fd
MinhBui2002 <heribertschneller@gmail.com>
2022-10-18 15:58:35 +0700

    fix some variable name

--- controllers/processes.py
+++ controllers/processes.py
@@ -2,21 +2,21 @@
 from models.modelProcess import Process, startAllProcesses, startProcessByName, stopAllProcesses, stopProcessByName
 
 #get all processes info
-def get_allProcesses_model():
+def get_all_processes_model():
     return Process.getAllProcessInfo()
 
 #start all processes, return array result
-def start_allProcesses_model():
+def start_all_processes_model():
     return startAllProcesses()    
 
 #start process by name, always return True unless error
-def startProcess_byName_model(name):
+def start_process_by_name_model(name):
     return startProcessByName(name)
 
 #stop process by name, always return True unless error
-def stopProcess_byName_model(name):
+def stop_process_by_name_model(name):
     return stopProcessByName(name)
 
 #stop ALL process, return array result
-def stop_allProcesses_model():
+def stop_all_processes_model():
     return stopAllProcesses()
