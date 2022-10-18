3e7b3c847c74003e0f4b0a2b255dcda7f54180be
MinhBui2002 <heribertschneller@gmail.com>
2022-10-18 22:41:30 +0700

    fix logging config and add routes

--- controllers/utils.py
+++ controllers/utils.py
@@ -4,8 +4,11 @@ def get_date():
     today = date.today()
     return today.strftime("%d-%m-%Y")
     
-# check the storage/logs folder and create the folder with the date of today if it doesn't exist
+# check the storage/logs folder and create the folder with the name of the date of today using get_date() function
 def check_logs_folder():
     import os
-    if not os.path.exists('storage/logs/'+get_date()):
-        os.makedirs('storage/logs/'+get_date())
+    if not os.path.exists('storage/logs/' + get_date()):
+        os.makedirs('storage/logs/' + get_date())
+    else:
+        pass
+    
