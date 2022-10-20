e407e5aff11413f69204079e5dbcb4638298a0b5
HoangLong <hoanglong.cmb@gmail.com>
2022-10-20 15:12:57 +0700

    get data from logfile, stdOut, stdErr

--- controllers/processes.py
+++ controllers/processes.py
@@ -1,5 +1,5 @@
 
-from models.modelProcess import Process, startAllProcesses, startProcessByName, stopAllProcesses, stopProcessByName
+from models.modelProcess import Process, allData_stdErr_logFile, allData_stdOut_logFile, startAllProcesses, startProcessByName, stopAllProcesses, stopProcessByName, clear_process_log
 
 #get all processes info
 def get_all_processes_model():
@@ -20,3 +20,15 @@ def stop_process_by_name_model(name):
 #stop ALL process, return array result
 def stop_all_processes_model():
     return stopAllProcesses()
+
+#get all data StdOut  of process by name
+def get_all_stdOut_process_model(name):
+    return allData_stdOut_logFile(name)
+
+#get all data StdErr  of process by name
+def get_all_stdErr_process_model(name):
+    return allData_stdErr_logFile(name)
+
+# clear stdOut, stdErr of process by name
+def clear_log_process_by_name_model(name):
+    return clear_process_log(name)
