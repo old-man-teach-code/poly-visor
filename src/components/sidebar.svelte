961a36a7018fc4df5d855a66bee72ad99980d3b6
HieuTVGCH200824 <hieutvgch200824@fpt.edu.vn>
2022-10-21 10:00:51 +0700

    sidebar layout update

--- src/components/sidebar.svelte
+++ src/components/sidebar.svelte
@@ -1,14 +1,15 @@
 <div class="bg-sidebar h-screen w-60">
-    <div class="flex p-10">
-        <a href="#a"><img class="w-8 h-8" src="src/images/logo.png" alt=""></a>
-        <h1 class="m-1 ml-3 text-text-logo text-xl">Poly-Visor</h1>
+    <div class="inline-flex p-10 w-full">
+        <img class="w-8 h-8" src="src/images/logo.png" alt="">
+        <h1 class="ml-3 text-text-logo text-xl">Poly-Visor</h1>
     </div>
-    <div class="p-10">
-        <a href="#overview" class="text-white text-sm p-2 flex"><img class=" mr-2 w-5 h-5" src="src/images/graph.png" alt="">Overview</a>
-        <a href="#overview" class="text-white text-sm p-2 flex"><img class=" mr-2 w-5 h-5" src="src/images/process.png" alt="">Processes</a>
-    </div>
-    <div class="mt-48 h-px bg-gray-600"></div>
-    <div class="p-10 pt-7">
+    <ul class="p-10">
+        <li><a href="#overview" class="text-white text-sm p-2 flex"><img class=" mr-2 w-5 h-5" src="src/images/graph.png" alt="">Overview</a></li>
+        <li><a href="#overview" class="text-white text-sm p-2 mt-5 flex"><img class=" mr-2 w-5 h-5" src="src/images/process.png" alt="">Processes</a></li>
+    </ul>
+    <div class="h-px bg-gray-600 mt-48"></div>
+    
+    <div class="p-10 pt-8">
         <a href="#setting" class="text-white text-sm p-2 flex"><img class=" mr-2 w-5 h-5" src="src/images/setting.png" alt="">Setting</a>
     </div>
 </div>
