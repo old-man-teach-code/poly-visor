be3ffbc2aacd148cda478c2e6d5b8eedc6812d87
HieuTVGCH200824 <hieutvgch200824@fpt.edu.vn>
2023-02-06 11:02:00 +0700

    eventSource close

--- app.py
+++ app.py
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
-from controllers.routes import app_routes
-from controllers.api import app_api
+from polyvisor.controllers.routes import app_routes
+from polyvisor.controllers.api import app_api
 from flask_cors import CORS
 from flask import Flask
-from controllers.utils import check_logs_folder
-check_logs_folder()
-=======
 from polyvisor.controllers.utils import check_logs_folder
 check_logs_folder()
-from flask import Flask
-from flask_cors import CORS
-from polyvisor.controllers.api import app_api
-from polyvisor.controllers.routes import app_routes
-
->>>>>>> 7f34956d7136ed484ce55dd2593dbb9a083c7f16
 
 
-app = Flask(__name__,static_folder="./build")
+app = Flask(__name__, static_folder="./build")
 CORS(app)
 app.register_blueprint(app_api)
 app.register_blueprint(app_routes)
