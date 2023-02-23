1d0d06266cb99fae08047adfea118689d9178891
HoangLong <hoanglong.cmb@gmail.com>
2023-02-23 22:17:41 +0700

    update default bind when not found  value config

--- polyvisor/rpc.py
+++ polyvisor/rpc.py
@@ -11,12 +11,14 @@ def make_rpc_interface(supervisord,**config):
     sup = SupervisorNamespaceRPCInterface(supervisord)
     
     #p1=threading.Thread(target=run_server)
-    bind = config.get("bind",0)
+    get_bind = config.get("bind",0)
+    bind = int(get_bind)
+    if(bind==0):
+        bind=5000
     p1=multiprocessing.Process(target=run_server,args=(bind,))    
     p1.start()
     
 
 def run_server(bind):   
-    http_server = WSGIServer(("127.0.0.1", int(bind)), app)
+    http_server = WSGIServer(("127.0.0.1", bind), app)
     http_server.serve_forever()
-
