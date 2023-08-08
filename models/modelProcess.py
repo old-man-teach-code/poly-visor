74474f78f3ac1519f53a2a06d4d917bb1279f200
MinhBui2002 <heribertschneller@gmail.com>
2023-08-08 09:47:00 +0700

    push

--- models/modelProcess.py
+++ models/modelProcess.py
@@ -138,7 +138,7 @@ def get_process_affinity_CPU(pid):
         
         char_index= output.find(":")
         output=output[char_index+2::].replace('\n','')
-        if '-' in output:
+        if ('-' or ',') in output:
             start, end = map(int, output.split('-'))
             return '\n'.join(str(i) for i in range(start, end + 1))
         else:
