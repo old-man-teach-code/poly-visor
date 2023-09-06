518d12fae25d95161addea0782006a8142ffad31
hoanglong <hoanglong.cmb@gmail.com>
2023-09-06 08:53:59 +0700

    auto for access point

--- polyvisor/rpc.py
+++ polyvisor/rpc.py
@@ -11,11 +11,11 @@ def make_rpc_interface(supervisord,**config):
     sup = SupervisorNamespaceRPCInterface(supervisord)
     
     #p1=threading.Thread(target=run_server)
-    get_bind = config.get("bind",0)
-    access_point = config.get("access_point","localhost")
-    bind = int(get_bind)
-    if(bind==0):
-        bind=5000
+    get_bind = config.get("bind",5000)
+    access_point = config.get("access_point","localhost")    
+    if access_point == "auto":        
+        access_point = get_ip()       
+    bind = int(get_bind)   
     p1=multiprocessing.Process(target=run_server,args=(access_point,bind,))    
     p1.start()
     
@@ -25,12 +25,13 @@ def run_server(ipAddress,bind):
     http_server.serve_forever()
 
 def get_ip():
-    import socket
-    # get ipv4 and ipv6 address of machine using socket module
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    ipv4 = s.getsockname()[0]
-    s.close()
-    return ipv4
-
-
+    try:
+        import socket
+        # get ipv4 and ipv6 address of machine using socket module
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ipv4 = s.getsockname()[0]
+        s.close()
+        return ipv4
+    except:
+        return "localhost"
