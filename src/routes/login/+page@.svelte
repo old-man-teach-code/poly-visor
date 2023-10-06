820c54624a7ec4959d77069a8c6a39ca989d23c6
HieuTVGCH200824 <HieuTVGCH200824@fpt.edu.vn>
2023-10-06 14:09:50 +0700

    ui: create login page

---
+++ src/routes/login/+page@.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+</script>
+
+<div class="bg-[#06113C] w-screen h-screen flex flex-col">
+	<h1 class="font-inter text-[32px] text-[#EEEEEE] font-bold mb-10 text-center bottom-0 mt-24">
+		Login
+	</h1>
+	<div class="bg-white mx-auto mt-14 rounded-2xl">
+		<div class="bg-[#FF8C32] rounded-tl-2xl rounded-tr-2xl flex justify-center py-6">
+			<span class="text-[#06113C] text-2xl font-bold">Polyvisor lid001</span>
+		</div>
+		<div class="px-20 py-10 space-y-6">
+			<div class="flex flex-row justify-between items-center gap-5">
+				<label for="username">Username</label>
+				<input type="text" name="username" id="username" />
+			</div>
+			<div class="flex flex-row justify-between items-center gap-5">
+				<label for="password">Password</label>
+				<input type="password" name="password" id="password" />
+			</div>
+		</div>
+	</div>
+</div>
