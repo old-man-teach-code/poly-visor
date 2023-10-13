a6e05e8ff8c8ff62ab0e1d17dbcbb2b275808b61
HieuTVGCH200824 <hieutvgch200824@fpt.edu.vn>
2023-10-13 20:40:07 +0700

    fix: check authenticated

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,7 +3,7 @@
 	import { getAllSupervisors } from '../../store/action';
 	import LoadingScreen from '../../components/LoadingScreen.svelte';
 	import LoginModal from '../../components/LoginModal.svelte';
-	import { isAuthenticated } from '../../store/supstore';
+	import { isAuthenticated, currentSupervisor } from '../../store/supstore';
 	interface Supervisor {
 		host: string;
 		name: string;
@@ -23,15 +23,16 @@
 	});
 
 	function handleSupervisorClick(supervisor: Supervisor) {
+		isAuthenticated.set(true);
+		currentSupervisor.set(supervisor.name);
 		if (!supervisor.authentication) {
-			window.location.href = `/login/${supervisor.name}`;
+			window.location.href = '/';
 			return;
 		}
 		if (!supervisor.running) {
 			alert('Supervisor is not running, please enable this supervisor instance!');
 			return;
 		}
-		selectedSupervisor = supervisor.name;
 		loginModal = true;
 	}
 </script>
@@ -52,7 +53,7 @@
 				>
 					<div class=" flex flex-row items-center gap-3 truncate">
 						<span class="text-base md:text-lg font-semibold truncate"
-							>Polyvisor {supervisor?.name}aaaaaaaaaaaaaaaaaaaaaaaaa</span
+							>Polyvisor {supervisor?.name}</span
 						>
 						<div
 							class="h-3 w-3 min-w-[12px] min-h-[12px] {supervisor?.running
